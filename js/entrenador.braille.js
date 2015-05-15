6febc0350e40b7a2871a62e0a809f9cfd308988c
Lucas Capalbo Lavezzo <lucas.capalbo@gmail.com>
2015-05-15 08:58:38 -0300

    BUGFIX: Escape numérico es @, no ~. BUGFIX: @ sin modo numérico activo es @, no escape numérico. Refactoring simplificando a es.js, llegare a una clase Braillear eventualmente. Agrego funciones de conversión texto latino a braille y viceversa. No consideran modos por ahora, primer aproximación

--- js/entrenador.braille.js
+++ js/entrenador.braille.js
@@ -1,29 +1,106 @@
+// Constantes
+var INICIO_TABLA_BRAILLE_UNICODE = 0x2800;
+var SALTO_LINEA = -1;
+var SALTO_LINEA_CHAR = "\n";
+var SALTO_LINEA_CHARCODE = SALTO_LINEA_CHAR.charCodeAt(0);
+// Mapa con valores estandar (se completa en archivos de maps)
+var map = [];
+map[SALTO_LINEA] = SALTO_LINEA_CHAR;
+map[0] = " ";
+// Mapa inverso (se inicializa solo)
+var invertedMap = {};
+// Modos de ingreso segun caracteres de comandos ingresados
+var modoMayuscula = 0, modoNumerico = 0, modoNumericoInterrupcion = 0;
+
+
+function invertirArray(array) {
+    var ret = {};
+    ret[ SALTO_LINEA_CHAR ] = SALTO_LINEA;
+
+    $(array).each(function (key, value) {
+        ret[value] = key;
+    });
+
+    return ret;
+}
+
+function convertirEnBraille(latino) {
+    var brailleString = "", largo = latino.length;
+    for (var pos = 0; pos < largo; pos++) {
+        var caracterLatino = latino.charAt(pos);
+        var valorBraille = invertedMap[caracterLatino];
+        brailleString = brailleString + obtenerCaracterBraille(valorBraille);
+    }
+    return brailleString;
+}
+
+function convertirEnLatino(braille) {
+    var latinoString = "", largo = braille.length;
+    for (var pos = 0; pos < largo; pos++) {
+        var charCode = braille.charCodeAt(pos);
+        var valorBraille = (charCode === SALTO_LINEA_CHARCODE)
+                ? SALTO_LINEA
+                : braille.charCodeAt(pos) - INICIO_TABLA_BRAILLE_UNICODE;
+        latinoString = latinoString + obtenerCaracterLatino(valorBraille);
+    }
+    return latinoString;
+}
+
+// Para probar solamente..Sacado de MDN, funciona pero debe ser invocado desde un handler
+function toggleFullScreen() {
+    if (!document.fullscreenElement && // alternative standard method
+            !document.mozFullScreenElement && !document.webkitFullscreenElement && !document.msFullscreenElement) {  // current working methods
+        if (document.documentElement.requestFullscreen) {
+            document.documentElement.requestFullscreen();
+        } else if (document.documentElement.msRequestFullscreen) {
+            document.documentElement.msRequestFullscreen();
+        } else if (document.documentElement.mozRequestFullScreen) {
+            document.documentElement.mozRequestFullScreen();
+        } else if (document.documentElement.webkitRequestFullscreen) {
+            document.documentElement.webkitRequestFullscreen(Element.ALLOW_KEYBOARD_INPUT);
+        }
+    } else {
+        if (document.exitFullscreen) {
+            document.exitFullscreen();
+        } else if (document.msExitFullscreen) {
+            document.msExitFullscreen();
+        } else if (document.mozCancelFullScreen) {
+            document.mozCancelFullScreen();
+        } else if (document.webkitExitFullscreen) {
+            document.webkitExitFullscreen();
+        }
+    }
+}
+
+
+// Variables propias de la interfaz
 var $valorBraille, $textoBraille, $valorLatino, $textoLatino, valor;
 
 /////////////////////////////////////////////////
 // simplificar esto, aunque sea multitouch se procesa
 // secuencialmente en JS así que no hay race conditions
 // podría usar int nomas, esto fue una prueba y quedó así
-var presiono = [0,0,0,0,0,0], solto = [0,0,0,0,0,0];
-function obtenerValor(){
-    var resultado=0;
-    for(var i=0; i<6; i++) {
-        if(presiono[i]) {
-           resultado += Math.pow(2,i);
+var presiono = [0, 0, 0, 0, 0, 0], solto = [0, 0, 0, 0, 0, 0];
+function obtenerValor() {
+    var resultado = 0;
+    for (var i = 0; i < 6; i++) {
+        if (presiono[i]) {
+            resultado += Math.pow(2, i);
         }
     }
     return resultado;
 }
 
-function sumar(array){
-    var resultado=0;
-    for(var i=0; i<6; i++) resultado += array[i] * Math.pow(2,i);
+function sumar(array) {
+    var resultado = 0;
+    for (var i = 0; i < 6; i++)
+        resultado += array[i] * Math.pow(2, i);
     return resultado;
 }
 
 function iguales(arrayA, arrayB) {
-    for(var i=0; i<6; i++) {
-        if(arrayA[i] != arrayB[i]) {
+    for (var i = 0; i < 6; i++) {
+        if (arrayA[i] !== arrayB[i]) {
             return false;
         }
     }
@@ -31,21 +108,25 @@ function iguales(arrayA, arrayB) {
 }
 
 function limpiar(arrayA, arrayB) {
-    for(var i=0; i<6; i++) {
+    for (var i = 0; i < 6; i++) {
         arrayA[i] = 0;
         arrayB[i] = 0;
     }
 }
-///////////////////////////////////////////////// 
+/////////////////////////////////////////////////
 
 /**
  * Devuelve el caracter Unicode del set Braille (desde \u2800).
  * Uso solo los primeros 64 caracteres, luego siguen de 8 puntos (para japonés/chino).
+ *
+ * @param int valor     Código braille del caracter; suma del valor de cada
+ * punto activo (punto p1=1, p2=2, p3=4, p4=8, p5=16, p6=32), o valor especial
+ * SALTO_LINEA para retorno de carro.
  */
-function obtenerCaracterBraille(codigo) {
-    return codigo == SALTO_LINEA 
-            ? "\n" 
-            : String.fromCharCode(10240 + codigo);
+function obtenerCaracterBraille(valor) {
+    return valor === SALTO_LINEA
+            ? SALTO_LINEA_CHAR
+            : String.fromCharCode(INICIO_TABLA_BRAILLE_UNICODE + valor);
 }
 
 
@@ -58,7 +139,7 @@ function presiona(btn) {
     $valorBraille.text(obtenerCaracterBraille(valor));
     $valorLatino.text(obtenerCaracterLatino(valor));
 }
-function presionaBoton(evt) {
+function presionaBoton() {
     presiona($(this));
 }
 
@@ -67,79 +148,86 @@ function aceptarCaracter(valor) {
     var caracterBraille = obtenerCaracterBraille(valor);
     var caracterLatino = obtenerCaracterLatino(valor);
 
-    if(caracterLatino == "^") {
+    if (caracterLatino === "^") {
         modoMayuscula++;
-        switch(modoMayuscula) {
-            case 1: console.log("modo mayúscula simple"); break;
-            case 2: console.log("modo mayúscula múltiple"); break;
-            default: 
-                modoMayuscula=0; 
+        switch (modoMayuscula) {
+            case 1:
+                console.log("modo mayúscula simple");
+                break;
+            case 2:
+                console.log("modo mayúscula múltiple");
+                break;
+            default:
+                modoMayuscula = 0;
                 console.log("quito modo mayúscula por abuso de signo");
         }
-    } else if(modoMayuscula == 1) {
-        modoMayuscula=0;
+    } else if (modoMayuscula === 1) {
+        modoMayuscula = 0;
         console.log("finalizo modo mayúscula un caracter");
-    } else if(modoMayuscula == 2 && caracterLatino == caracterLatino.toLowerCase()) {
-        modoMayuscula=0;
+    } else if (modoMayuscula === 2 && caracterLatino === caracterLatino.toLowerCase()) {
+        modoMayuscula = 0;
         console.log("finalizo modo mayúscula múltiple, limita con: " + caracterLatino);
     }
-    
-    if(caracterLatino == "#") {
+
+    if (caracterLatino === "#") {
         modoNumerico++;
-        switch(modoNumerico) {
-            case 1: console.log("modo numérico"); break;
-            default: 
-                modoNumerico=0; 
-                modoNumericoInterrupcion=0;
+        switch (modoNumerico) {
+            case 1:
+                console.log("modo numérico");
+                break;
+            default:
+                modoNumerico = 0;
+                modoNumericoInterrupcion = 0;
                 console.log("quito modo numérico por abuso de signo");
         }
-    } else if(modoNumerico) {    
-        if(caracterLatino == "~") {    
-            modoNumericoInterrupcion++
+    } else if (modoNumerico) {
+        if (caracterLatino === "@") {
+            modoNumericoInterrupcion++;
             console.log("interrumpo modo numérico");
-        } else if(modoNumericoInterrupcion) {
-            modoNumericoInterrupcion=0;
+        } else if (modoNumericoInterrupcion) {
+            modoNumericoInterrupcion = 0;
             console.log("reactivo modo numérico");
-        } else if(caracterLatino == " " || caracterLatino == "\n") {
-            modoNumerico=0;
-            modoNumericoInterrupcion=0;
+        } else if (caracterLatino === " " || caracterLatino === SALTO_LINEA_CHAR) {
+            modoNumerico = 0;
+            modoNumericoInterrupcion = 0;
             console.log("desactivo modo numérico");
         }
     }
-    
+
     var colorLatino = "", titulo = "";
-    if(caracterLatino == " ") { // mayús
+    if (caracterLatino === " ") {        // espacio
         caracterLatino = "&nbsp;";
-    } else if(caracterLatino == "^") { // mayús
-        if(modoMayuscula == 1) {
+    } else if (caracterLatino === "^") { // mayús
+        if (modoMayuscula === 1) {
             titulo = "Modo mayúscula";
             colorLatino = "colorMayus";
         } else {
             titulo = "Modo capital";
             colorLatino = "colorCapital";
         }
-    } else if(caracterLatino == "#") { // num
+    } else if (caracterLatino === "#") { // num
         colorLatino = "colorNum";
         titulo = "Modo numérico";
-    } else if(caracterLatino == "~") { // escape num
-        caracterLatino = "#";          // lo mostramos como # más claro
+    } else if (caracterLatino === "@" && modoNumerico) { // escape num
         colorLatino = "colorEscapeNum";
         titulo = "Escape modo numérico";
-    } else if(caracterLatino == "") {  // no mapeado, desconocido
+    } else if (caracterLatino === "") {  // no mapeado, desconocido
         caracterLatino = "?";
-        titulo = "Carater braille desconocido";
+        titulo = "Caracter braille desconocido";
         colorLatino = "colorDesconocido";
     }
 
-    if(titulo) titulo = ' title="' + titulo + '" ';
-    
-    if(caracterLatino == "\n") {
+    if (titulo) {
+        titulo = ' title="' + titulo + '" ';
+    }
+
+    if (caracterLatino === SALTO_LINEA_CHAR) {
         caracterBraille = caracterLatino = '<span><br/><span class="spanCaracter">&nbsp;</span></span>';
     } else {
         caracterBraille = '<span class="spanCaracter">' + caracterBraille + '</span>';
         caracterLatino = '<span ' + titulo + ' class="spanCaracter ' + colorLatino + '">' + caracterLatino + '</span>';
     }
-    
+
     $textoBraille.append(caracterBraille);
     $textoLatino.append(caracterLatino);
 }
@@ -148,37 +236,38 @@ function aceptarCaracter(valor) {
 function suelta(btn) {
     var idx = btn.data('idx');
     solto[idx] = 1;
-    if(iguales(presiono, solto)) {
+    if (iguales(presiono, solto)) {
         aceptarCaracter(valor);
         limpiar(presiono, solto);
-        valor=0;
+        valor = 0;
         $valorBraille.text('');
         $valorLatino.text('');
     }
 }
-function sueltaBoton(evt) {
-    suelta($(this))
+function sueltaBoton() {
+    suelta($(this));
 }
 
 
 function agregaEspacio() {
-    if(!obtenerValor()) {
+    if (!obtenerValor()) {
         aceptarCaracter(0);
     }
 }
 
 
 function agregaSaltoLinea() {
-    if(!obtenerValor()){
+    if (!obtenerValor()) {
         aceptarCaracter(SALTO_LINEA);
-        $('#textoBrailleContainer').animate({ scrollTop: $textoBraille.height() });
-        $('#textoLatinoContainer').animate({ scrollTop: $textoLatino.height() });
+        //TODO: podría enlazar el handler scroll de uno e invocar al otro? para mantenerlo siempre sinc.
+        $('#textoBrailleContainer').animate({scrollTop: $textoBraille.height()});
+        $('#textoLatinoContainer').animate({scrollTop: $textoLatino.height()});
     }
 }
 
 
 function borrarUltimoCaracter() {
-    if(!obtenerValor() && $textoBraille.contents().length>1) {        
+    if (!obtenerValor() && $textoBraille.contents().length > 1) {
         $textoBraille.contents().last().remove();
         $textoLatino.contents().last().remove();
     }
@@ -186,24 +275,47 @@ function borrarUltimoCaracter() {
 
 
 function eventoTeclaComando(evt) {
-    if(obtenerValor()) return;
-    if(evt.key == " ") agregaEspacio();
-    if(evt.keyCode == 13) agregaSaltoLinea();
-    if(evt.keyCode == 8) borrarUltimoCaracter();
+    if (obtenerValor())
+        return;
+    if (evt.key === " ")
+        agregaEspacio();
+    if (evt.keyCode === 13)
+        agregaSaltoLinea();
+    if (evt.keyCode === 8)
+        borrarUltimoCaracter();
 }
 
 
 function eventoTecla(handler) {
-    return function(e) {
-        if(!e.key) return;
+    return function (e) {
+        if (!e.key)
+            return;
         switch (e.key) {
-            case "Q": case "q": handler($('#btnQ')); break;
-            case "A": case "a": handler($('#btnA')); break;
-            case "Z": case "z": handler($('#btnZ')); break;
-            case "O": case "o": handler($('#btnO')); break;
-            case "K": case "k": handler($('#btnK')); break;
-            case "M": case "m": handler($('#btnM')); break;
-            break;
+            case "Q":
+            case "q":
+                handler($('#btnQ'));
+                break;
+            case "A":
+            case "a":
+                handler($('#btnA'));
+                break;
+            case "Z":
+            case "z":
+                handler($('#btnZ'));
+                break;
+            case "O":
+            case "o":
+                handler($('#btnO'));
+                break;
+            case "K":
+            case "k":
+                handler($('#btnK'));
+                break;
+            case "M":
+            case "m":
+                handler($('#btnM'));
+                break;
+                break;
         }
         return;
     }
@@ -219,35 +331,10 @@ function limpiarTodo() {
 }
 
 
-// Para probar solamente..
-function toggleFullScreen() {
-  if (!document.fullscreenElement &&    // alternative standard method
-      !document.mozFullScreenElement && !document.webkitFullscreenElement && !document.msFullscreenElement ) {  // current working methods
-    if (document.documentElement.requestFullscreen) {
-      document.documentElement.requestFullscreen();
-    } else if (document.documentElement.msRequestFullscreen) {
-      document.documentElement.msRequestFullscreen();
-    } else if (document.documentElement.mozRequestFullScreen) {
-      document.documentElement.mozRequestFullScreen();
-    } else if (document.documentElement.webkitRequestFullscreen) {
-      document.documentElement.webkitRequestFullscreen(Element.ALLOW_KEYBOARD_INPUT);
-    }
-  } else {
-    if (document.exitFullscreen) {
-      document.exitFullscreen();
-    } else if (document.msExitFullscreen) {
-      document.msExitFullscreen();
-    } else if (document.mozCancelFullScreen) {
-      document.mozCancelFullScreen();
-    } else if (document.webkitExitFullscreen) {
-      document.webkitExitFullscreen();
-    }
-  }
-}
+$(function () {
+    inicializarMapa(map);
+    invertedMap = invertirArray(map);
 
-
-
-$(function() {
     $valorBraille = $('#valorBraille');
     $valorLatino = $('#valorLatino');
     $textoBraille = $('#textoBraille');
@@ -256,17 +343,17 @@ $(function() {
 
     $(window).keydown(eventoTecla(presiona));
     $(window).keyup(eventoTecla(suelta));
-    $.each($('.btnBraile'), function(idx, btn){
+    $.each($('.btnBraile'), function (idx, btn) {
         btn.addEventListener('touchstart', presionaBoton, false);
         btn.addEventListener('touchend', sueltaBoton, false);
     });
-    
+
     $(window).keyup(eventoTeclaComando);
     $('#btnEspacio').click(agregaEspacio);
     $('#btnSaltoLinea').click(agregaSaltoLinea);
     $('#btnBorrar').click(borrarUltimoCaracter);
 //    $('#btnLimpiar').click(limpiarTodo);
     aceptarCaracter(0);
-    
+
     $valorBraille.click(toggleFullScreen);
 });
--- js/maps/es.js
+++ js/maps/es.js
@@ -1,87 +1,97 @@
-SALTO_LINEA = -1;
-modoMayuscula=0, modoNumerico=0, modoNumericoInterrupcion=0;
-map = [];
-
-// comandos
-map[SALTO_LINEA] = "\n";
-map[0] = " ";
-
-// modo mayúscula, puntos 46
-map[40] = "^";
-// modo numérico, puntos 3456
-map[60] = "#";
-// interrupción de modo numérico por un único caracter, punto 5
-map[16] = "~";
-
-// letras
-map[1] = "a";
-map[3] = "b";
-map[9] = "c";
-map[25] = "d";
-map[17] = "e";
-map[11] = "f";
-map[27] = "g";
-map[19] = "h";
-map[10] = "i";
-map[26] = "j";
-map[5] = "k";
-map[7] = "l";
-map[13] = "m";
-map[29] = "n";
-map[59] = "ñ";
-map[21] = "o";
-map[15] = "p";
-map[31] = "q";
-map[23] = "r";
-map[14] = "s";
-map[30] = "t";
-map[37] = "u";
-map[39] = "v";
-map[58] = "w";
-map[45] = "x";
-map[59] = "y";
-map[53] = "z";
-map[55] = "á";
-map[46] = "é";
-map[12] = "í";
-map[44] = "ó";
-map[62] = "ú";
-map[51] = "ü";
-
-// signos ortograficos 
-map[4] = ".";
-map[2] = ",";
-map[6] = ";";
-map[18] = ":";
-map[35] = "(";
-map[28] = ")";
-map[55] = "[";
-map[62] = "]";
-map[36] = "-";
-map[20] = "*";
-
-// signos emparejados, apertura y cierre
-//case 34: caracter = "¿ ?";
-//case 21: caracter = "¡ !";
-//case 38: caracter = "“ ”";
-   
 /**
- * Devuelve el caracter latino asociado al código braille
+ * Inicializa el mapeo de valores (suma del valor de los puntos braille p1..p6
+ * como binario 1..32) al caracter latino equivalente.
+ *
+ * @param array map Mapa valor braille->caracter latino inicial.
  */
-function obtenerCaracterLatino(codigo) {
-    var caracter = map[codigo] || "";
-    
-    if(modoMayuscula) {
+function inicializarMapa(map) {
+    // comandos
+    // modo mayúscula, puntos 46
+    map[40] = "^";
+    // modo numérico, puntos 3456
+    map[60] = "#";
+    // interrupción de modo numérico por un único caracter, punto 5
+    map[16] = "@";
+
+    // abecedario
+    map[1] = "a";
+    map[3] = "b";
+    map[9] = "c";
+    map[25] = "d";
+    map[17] = "e";
+    map[11] = "f";
+    map[27] = "g";
+    map[19] = "h";
+    map[10] = "i";
+    map[26] = "j";
+    map[5] = "k";
+    map[7] = "l";
+    map[13] = "m";
+    map[29] = "n";
+    map[21] = "o";
+    map[15] = "p";
+    map[31] = "q";
+    map[23] = "r";
+    map[14] = "s";
+    map[30] = "t";
+    map[37] = "u";
+    map[39] = "v";
+    map[58] = "w";
+    map[45] = "x";
+    map[59] = "y";
+    map[53] = "z";
+
+    // acentos y letras español
+    map[55] = "á";
+    map[46] = "é";
+    map[12] = "í";
+    map[44] = "ó";
+    map[62] = "ú";
+    map[51] = "ü";
+    map[59] = "ñ";
+
+    // signos ortograficos
+    map[4] = ".";
+    map[2] = ",";
+    map[6] = ";";
+    map[18] = ":";
+    map[35] = "(";
+    map[28] = ")";
+    map[55] = "[";
+    map[62] = "]";
+    map[36] = "-";
+    map[20] = "*";  // TODO: * también se usa como apertura y cierre de enfasis, para texto subrayado, negrita o itálica. No sé como diferencian. El multiplicar es otro signo
+
+    // TODO: signos emparejados, apertura y cierre
+    //case 34: caracter = "¿ ?";
+    //case 21: caracter = "¡ !";
+    //case 38: caracter = "“ ”";
+
+    // TODO: completar signos matemáticos y otros
+    // * arroba es escape de modo numérico, ya conf. arriba
+    // * numeral solo es indicador de modo numérico. El numeral braille es
+    // combinación #@ (TODO incluir soporte, #@a sería #a, no a ni 1)
+}
+
+
+/**
+ * Devuelve el caracter latino asociado al código braille, según el modo de
+ * entrada actual.
+ */
+function obtenerCaracterLatino(valor) {
+    var caracter = map[valor] || "";
+
+    if (modoMayuscula) {
         caracter = caracter.toUpperCase();
     }
 
-    if(modoNumerico && !modoNumericoInterrupcion) {
-        if(caracter == "j") {
+    if (modoNumerico && !modoNumericoInterrupcion) {
+        if (caracter === "j") {
             caracter = "0";
-        } else if(caracter>="a" && caracter<"j") {
+        } else if (caracter >= "a" && caracter < "j") {
             caracter = String.fromCharCode("1".charCodeAt(0) + caracter.charCodeAt(0) - "a".charCodeAt(0));
         }
     }
-    
+
     return caracter;
 }
